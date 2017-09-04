0baac2f96b4fcdc9129f6a88f854a6cb3bfe2534
Jimmy <z5163448@cse.unsw.edu.au>
2017-09-04 10:00:00 +1000

    Added login

--- main.py
+++ main.py
@@ -3,4 +3,25 @@ from server import app
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template("homepage.html")
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
+		return render_template("homepage.html")
+
+@app.route('/login', methods = ['POST'])
+def login():
+	if request.method == 'POST':
+		if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+			session['logged_in'] = True
+			return redirect(url_for("index"))
+		else:
+			flash('Invalid Login, Please try again.')
+			return index()
+	return redender_template('login.html')
+
+@app.route('/logout')
+def logout():
+	if not session.get('logged_in'):
+		return redirect(url_for('login'))
+	session.pop('username', None)
+	return redirect(url_for('index'))
